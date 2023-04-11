22da3c066ce0f75e5fd5c16b084b4bd4723f26a9
giospes <giovannijacopo.spes@gmail.com>
2023-04-12 00:12:09 +0200

    bonus

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const app = Vue.createApp({
     },
     methods:{
         generateEmails(){
+            this.emails=[]
             for(let i =0; i<this.maxNumber; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (res)=>{
                     this.emails[i] = res.data.response
