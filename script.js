bf59399ef63d0c120ef29136e47c6e4d4e3376b5
giospes <giovannijacopo.spes@gmail.com>
2023-04-11 15:02:30 +0200

    esercizio base fatto

---
+++ script.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            maxNumber: 10,
+            emails: []
+
+        }
+    },
+    methods:{
+        generateEmails(){
+            for(let i =0; i<this.maxNumber; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (res)=>{
+                    this.emails[i] = res.data.response
+                })
+                
+            }   
+        }   
+    }
+})
+
+app.mount("#app")
